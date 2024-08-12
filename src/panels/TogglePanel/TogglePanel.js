b89769ddfb3232b6680c72b40fd5155beb77aabf
Aayush Joglekar <aayushjog@gmail.com>
2024-08-12 22:12:16 +0530

    Add help tooltips on toggle buttons

--- src/panels/TogglePanel/TogglePanel.js
+++ src/panels/TogglePanel/TogglePanel.js
@@ -4,7 +4,7 @@ import IconButton from "@mui/material/IconButton";
 import TuneIcon from "@mui/icons-material/Tune";
 import EqualizerIcon from "@mui/icons-material/Equalizer";
 import BubbleChartIcon from "@mui/icons-material/BubbleChart";
-import { Stack } from "@mui/material";
+import { Stack, Tooltip } from "@mui/material";
 
 function TogglePanel({
   isControlPanelVisible,
@@ -17,33 +17,47 @@ function TogglePanel({
   return (
     <div className="toggle-panel">
       <Stack direction="row" spacing={0.5}>
-        <IconButton
-          sx={{ color: isControlPanelVisible ? "white" : "gray" }}
-          fontSize="small"
-          onClick={() => {
-            setIsControlPanelVisible((prev) => !prev);
-          }}
+        <Tooltip
+          title={`${isControlPanelVisible ? "Hide" : "Show"} controls`}
+          arrow
         >
-          <TuneIcon />
-        </IconButton>
-        <IconButton
-          sx={{ color: isStatsVisible ? "white" : "gray" }}
-          fontSize="small"
-          onClick={() => {
-            setIsStatsVisible((prev) => !prev);
-          }}
+          <IconButton
+            sx={{ color: isControlPanelVisible ? "white" : "gray" }}
+            fontSize="small"
+            onClick={() => {
+              setIsControlPanelVisible((prev) => !prev);
+            }}
+          >
+            <TuneIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title={`${isStatsVisible ? "Hide" : "Show"} statistics`} arrow>
+          <IconButton
+            sx={{ color: isStatsVisible ? "white" : "gray" }}
+            fontSize="small"
+            onClick={() => {
+              setIsStatsVisible((prev) => !prev);
+            }}
+          >
+            <EqualizerIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip
+          title={`${
+            spawnSeedsOnClick ? "Disable" : "Enable"
+          } seed spawning on mouse click`}
+          arrow
         >
-          <EqualizerIcon />
-        </IconButton>
-        <IconButton
-          sx={{ color: spawnSeedsOnClick ? "white" : "gray" }}
-          fontSize="small"
-          onClick={() => {
-            setSpawnSeedsOnClick((prev) => !prev);
-          }}
-        >
-          <BubbleChartIcon />
-        </IconButton>
+          <IconButton
+            sx={{ color: spawnSeedsOnClick ? "white" : "gray" }}
+            fontSize="small"
+            onClick={() => {
+              setSpawnSeedsOnClick((prev) => !prev);
+            }}
+          >
+            <BubbleChartIcon />
+          </IconButton>
+        </Tooltip>
       </Stack>
     </div>
   );
